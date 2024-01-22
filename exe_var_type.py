e027fc3f9f374cf9c9a046d0ab266fde9405b05e
Moiseis Sebastian <mois3is@hotmail.com>
2024-01-22 11:55:59 -0300

    third test commit

---
+++ exe_var_type.py
@@ -0,0 +1,13 @@
+name = 'Moiseis'
+surname = 'Sebastian'
+age = 26
+birth_year = 1997
+of_legal_age = age >= 18
+height_meters = 1.80
+
+print('Name:',name)
+print('Surname:', surname)
+print('Age:', age)
+print('Birth Year:', birth_year)
+print('Are you of legal age?', of_legal_age)
+print('Height in meters?', height_meters)
--- uso_print.py
+++ uso_print.py
@@ -3,3 +3,4 @@
 # The end command is used to inform what will be in the line break
 print(12, 34, sep='.', end="#\n")
 print(56, 78, sep="-")
+
