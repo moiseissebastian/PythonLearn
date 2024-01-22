ebb4a95bddfc2e8602409b04867c24f010ef11dc
Moiseis Sebastian <mois3is@hotmail.com>
2024-01-22 10:22:55 -0300

    Second commit test

---
+++ usoPrint.py
@@ -0,0 +1,5 @@
+# sep command is used to change the type of separator in python
+# the same class explains about line breaking with \r\n
+# The end command is used to inform what will be in the line break
+print(12, 34, sep='.', end="#\n")
+print(56, 78, sep="-")
