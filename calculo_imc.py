c1f3af58345115b3cd7674a6c3915b88b5005d50
Moiseis Sebastian <mois3is@hotmail.com>
2024-01-23 10:50:48 -0300

    primeiro commit 23.01.2024

--- calculo_imc.py
+++ calculo_imc.py
@@ -7,4 +7,4 @@ peso = int(input('Qual o seu peso? '))
 
 imc = peso / altura ** 2
 
-print(f'\n{nome} tem de {altura:.2f},\npesa {peso} quilos e seu IMC é\n{imc}')
+print(f'\n{nome} tem de {altura:.2f},\npesa {peso} quilos e seu IMC é\n{imc:.2f}')
